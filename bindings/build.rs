f77fe7cb10e86100426f3f4a0a5988e0af52b111
Andreas Rånman <rondreas@users.noreply.github.com>
2021-01-26 23:03:58 +0100

    Not knowing if I have the hInstance by default, opted to get it through GetModuleHandeA.

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     windows::build!(
-        windows::win32::system_services::{MB_OK, MB_ICONINFORMATION}
-        windows::win32::windows_and_messaging::MessageBoxA
+        windows::win32::system_services::{GetModuleHandleA, MB_OK, MB_ICONINFORMATION}
+        windows::win32::windows_and_messaging::{MessageBoxA, WNDCLASSA}
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,23 @@
 
 
 use bindings::{
-    windows::win32::system_services::{MB_OK, MB_ICONINFORMATION},
-    windows::win32::windows_and_messaging::{MessageBoxA, HWND},
+    windows::win32::system_services::{GetModuleHandleA, MB_OK, MB_ICONINFORMATION},
+    windows::win32::windows_and_messaging::{MessageBoxA, HWND, WNDCLASSA},
 };
 
 fn main() -> windows::Result<()> {
+
+    let window = WNDCLASSA::default();
+
+    unsafe { // Let's get the hInstance to set up our window
+        let h_instance = GetModuleHandleA(0 as *const i8);
+    }
+
+    // window.style = ;
+    // window.lpfnWndProc = ;
+    // window.hInstance = h_instance;
+    // window.hIcon = ;
+    // window.lplzClassName = "HandmadeHeroWindowClass";
 
     unsafe {
         let title = b"Hello\0";
