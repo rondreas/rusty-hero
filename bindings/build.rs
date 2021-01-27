8fd73876f45e61557ad1dc7730333dcdae0d8197
Andreas Rånman <rondreas@users.noreply.github.com>
2021-01-27 23:44:00 +0100

    We have a window
    Can now start the app and close the window :tada:

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,16 +1,23 @@
 fn main() {
     windows::build!(
+        windows::win32::menus_and_resources::HMENU
         windows::win32::system_services::{
             HINSTANCE, GetModuleHandleA,        // get the Handle to Instance typedef and the method for getting it,
             CS_OWNDC, CS_HREDRAW, CS_VREDRAW,   // get the Window Class Styles,
+            CW_USEDEFAULT,                      // default values for windows height, width and position
+            WS_OVERLAPPEDWINDOW, WS_VISIBLE,    // window styles,
             LRESULT,                            // signed result of message processing,
             WM_SIZE, WM_DESTROY, WM_CLOSE,      // enums for window notifications,
             WM_ACTIVATEAPP
         }
         windows::win32::windows_and_messaging::{
-            HWND, WNDCLASSA,                    // Get structs for Window Handle and Window Class,
+            HWND, WNDCLASSA,                    // handle for window and window class,
             LPARAM, WPARAM,                     // typedefined pointers for additional messages for window procedure,
-            DefWindowProcA                      // Default Window Procedure ( callback function )
+            DefWindowProcA,                     // Default Window Procedure ( callback function )
+            RegisterClassA,
+            CreateWindowExA,
+            MSG, GetMessageA, DispatchMessageA, // Messsage loop for our window,
+            DestroyWindow, PostQuitMessage
         }
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,48 @@
  */
 
 use bindings::{
+    windows::win32::menus_and_resources::HMENU,
     windows::win32::system_services::{
         HINSTANCE, GetModuleHandleA,        // get the Handle to Instance typedef and the method for getting it,
         CS_OWNDC, CS_HREDRAW, CS_VREDRAW,   // get the Window Class Styles,
+        CW_USEDEFAULT,                      // default values for windows height, width and position
+        WS_OVERLAPPEDWINDOW, WS_VISIBLE,    // window styles,
         LRESULT,                            // signed result of message processing,
         WM_SIZE, WM_DESTROY, WM_CLOSE,      // enums for window notifications,
         WM_ACTIVATEAPP
     },
     windows::win32::windows_and_messaging::{
-        HWND, WNDCLASSA,                    // Get structs for Window Handle and Window Class,
+        HWND, WNDCLASSA,                    // handle for window and window class,
         LPARAM, WPARAM,                     // typedefined pointers for additional messages for window procedure,
-        DefWindowProcA                      // Default Window Procedure ( callback function )
+        DefWindowProcA,                     // Default Window Procedure ( callback function )
+        RegisterClassA,
+        CreateWindowExA,
+        MSG, GetMessageA, DispatchMessageA, // Messsage loop for our window,
+        DestroyWindow, PostQuitMessage
     },
 };
 
 extern "system" fn wndproc (window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
     unsafe {
         match message as i32 {
+            WM_SIZE => {
+                println!("Changed size\n");
+                LRESULT(0)
+            }
+            WM_DESTROY => {
+                println!("Destroying window\n");
+                PostQuitMessage(0);
+                LRESULT(0)
+            }
+            WM_CLOSE => {
+                println!("Closing window\n");
+                DestroyWindow(window);
+                LRESULT(0)
+            }
+            WM_ACTIVATEAPP => {
+                println!("Activated app\n");
+                LRESULT(0)
+            }
             _ => DefWindowProcA(window, message, wparam, lparam),
         }
     }
@@ -50,6 +75,31 @@ fn main() -> windows::Result<()> {
         // Set the window class name, 
         let window_class_name = b"HandmadeHeroWindowClass\0";
         window.lpsz_class_name = window_class_name.as_ptr() as *mut i8;
+
+        let atom = RegisterClassA(&window);
+        std::debug_assert!(atom != 0);
+
+        let title = b"Handmade Hero\0";
+
+        CreateWindowExA(
+            0,
+            window_class_name.as_ptr() as *mut i8,
+            title.as_ptr() as *const i8,
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            HWND(0),
+            HMENU(0),
+            instance,
+            std::ptr::null_mut(), // LPVOID, long pointer void
+        );
+
+        let mut message = MSG::default();
+        while GetMessageA(&mut message, HWND(0), 0, 0).into() {
+            DispatchMessageA(&mut message);
+        }
 
         Ok(())
     }
